b6b5cf6afe2237d44df828fbcb1d6ece65ba5911
gurra99 <30boman@gmail.com>
2025-02-23 17:32:47 +0100

    Skeleton code for home page added.

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Header } from "./home.styles";
+
+const Home = () => {
+  return (
+    <Container>
+      <Header className="primary-heading">Evinova Tv-Series</Header>
+    </Container>
+  );
+};
+
+export default Home;
