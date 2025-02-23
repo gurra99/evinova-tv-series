45e0fa7011b0ed2a9aad5dc2f4c7ef5c01c225ca
gurra99 <30boman@gmail.com>
2025-02-23 17:26:38 +0100

    Root tanstack query and react-router added to index.

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
