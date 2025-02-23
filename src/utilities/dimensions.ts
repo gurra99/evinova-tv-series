76a3f8c000958bcfa5a73e92a2494f4243d2cf95
gurra99 <30boman@gmail.com>
2025-02-23 21:49:07 +0100

    Removed unused code.

--- src/utilities/dimensions.ts
+++ src/utilities/dimensions.ts
@@ -1,11 +1,5 @@
 export const dimensions = {
-    tablets: "600px",
-    mediumDevices: "801px",
-    largeDevices: "1025px",
-};
-
-export const dimensionsNumber = {
-    tablets: 600,
-    mediumDevices: 801,
-    largeDevices: 1025,
+  tablets: "600px",
+  mediumDevices: "801px",
+  largeDevices: "1025px",
 };
