4cd4b1c434f0bda8376323a3eafd2f37f6e491ce
gurra99 <30boman@gmail.com>
2025-02-23 21:06:13 +0100

    Added jest test.

--- src/tests/search.test.tsx
+++ src/tests/search.test.tsx
@@ -1,5 +1,34 @@
 import { screen } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import Search from "../pages/home/search/search";
+import {
+  customRender,
+  waitForSearchResults,
+} from "../utilities/test-config/test-setup";
 
-test("", () => {});
+test("h2 should be in search page", () => {
+  customRender(<Search />);
+  const headerElement = screen.getByRole("heading", { level: 2 });
+  expect(headerElement).toBeInTheDocument();
+});
+
+it("renders input field correctly", () => {
+  customRender(<Search />);
+  expect(screen.getByPlaceholderText("Search")).toBeInTheDocument();
+});
+
+test("should navigate to the correct TV series page when a search result is clicked", async () => {
+  customRender(<Search />);
+
+  const input = screen.getByPlaceholderText("Search");
+
+  await user.click(input);
+  await user.keyboard("Star Wars Andor");
+  await waitForSearchResults();
+
+  const link = screen.getByRole("link");
+  await user.click(link);
+
+  screen.logTestingPlaygroundURL();
+  expect(global.window.location.pathname).toContain("/tv-serie/52341");
+});
