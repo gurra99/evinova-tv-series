d678967fcd44160e82e0ed87b03704e0eed40e46
gurra99 <30boman@gmail.com>
2025-02-23 17:44:55 +0100

    Added code to fetch data from api.

--- src/api/tv-serie-api.ts
+++ src/api/tv-serie-api.ts
@@ -1,1 +1,12 @@
-export const fetchTvSerieByName = async () => {};
+import { baseUrl, fetchOptions } from "./config";
+
+export const fetchTvSerieByName = async (name: string | null) => {
+  const url = baseUrl + `/search/shows?q=${name}`;
+
+  const res = await fetch(url, {
+    ...fetchOptions("GET", "application/json"),
+  });
+
+  const json = await res.json();
+  return json;
+};
--- src/pages/home/search/search.tsx
+++ src/pages/home/search/search.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchTvSerieByName } from "../../../api/tv-serie-api";
 
 const Search = () => {
+  const {
+    data: tvSeries,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["tv-serie", "Star Wars"],
+    queryFn: async () => {
+      return await fetchTvSerieByName("Star Wars");
+    },
+  });
+
+  console.log(tvSeries);
+
   return;
 };
 
