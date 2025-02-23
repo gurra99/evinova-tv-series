0309618d4c50cdf59ee4a0621739c33db33b35e8
gurra99 <30boman@gmail.com>
2025-02-23 19:48:32 +0100

    Added skeleton code for tv-serie page.

---
+++ src/pages/tv-serie/tv-serie.styles.tsx
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+import { dimensions } from "../../utilities/dimensions";
+
+export const Container = styled.div``;
---
+++ src/pages/tv-serie/tv-serie.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router";
+import { Container } from "./tv-serie.styles";
+
+const TvSerie = () => {
+  return <Container></Container>;
+};
+
+export default TvSerie;
