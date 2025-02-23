893aeaf032c749af49481d044f975d59df2962c0
gurra99 <30boman@gmail.com>
2025-02-23 18:45:38 +0100

    Created a loading spinner.

---
+++ src/components/fragments/loading-spinner/loading-spinner.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Root, Text } from "./loading-spinner.styles";
+
+interface LoadingSpinnerProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  text?: string;
+}
+
+export function LoadingSpinner({ text = "Loading..." }: LoadingSpinnerProps) {
+  return (
+    <Root>
+      <Text>{text}</Text>
+    </Root>
+  );
+}
