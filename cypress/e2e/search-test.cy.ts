f8c39c2f90fa68961e22280ece5e5ad553a9fb40
gurra99 <30boman@gmail.com>
2025-02-23 20:29:00 +0100

    Cypress with a first test.

---
+++ cypress/e2e/search-test.cy.ts
@@ -0,0 +1,9 @@
+describe("search page tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("correct url", () => {
+    cy.visit("http://localhost:3000/");
+  });
+});
--- src/api/config.ts
+++ src/api/config.ts
@@ -1,10 +1,15 @@
 export const baseUrl = "https://api.tvmaze.com";
 
-export function fetchOptions(method: string, accept: "application/json") {
+export function fetchOptions(
+  method: string,
+  accept: "application/json",
+  signal?: AbortSignal,
+) {
   return {
     method: method,
     headers: {
       accept: accept,
     },
+    signal: signal,
   };
 }
